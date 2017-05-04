d5ff7d28bdff852cbd9523951659d1b75223818f
Young Bo Kim <yb82@naver.com>
2017-05-04 15:46:37 +1000

    add jquery post date

--- update.php
+++ update.php
@@ -7,7 +7,23 @@ define("SOSO",2);
 define("ANGRY",1);
 
 $record = new Record();
+if(isset($_POST["date"])){
 
+	$data = explode(",", $_POST['date']);
+	print_r($data);
+	echo $record->createRangeData($data[0],$data[1]);
+
+}
+if(isset($_POST["today"])){
+
+	echo $record->createTodaysandAllData(1);
+
+}
+if(isset($_POST["all"])){
+
+	echo $record->createTodaysandAllData(3);
+
+}
 if (isset ( $_POST ["emo"] )) {
 	$emo = $_POST["emo"];
 
